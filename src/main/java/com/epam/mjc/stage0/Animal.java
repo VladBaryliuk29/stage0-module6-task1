a77240ba314f8a849615b0332b445bad0c00c766
unknown <vladmain29@gmail.com>
2023-04-08 14:35:00 +0300

    Mjc tasks

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,31 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public Animal() {
+
+    }
+
+    public String getDescription() {
+        String reallyHas;
+        if (hasFur) {
+             reallyHas = "a";
+        } else {
+             reallyHas = "no";
+        }
+        if (numberOfPaws != 1) {
+            return "This animal is mostly " + color + ". It has " + numberOfPaws + " paws and " + reallyHas + " fur.";
+        } else {
+            return "This animal is mostly " + color + ". It has " + numberOfPaws + " paw and " + reallyHas + " fur.";
+        }
+    }
 }
